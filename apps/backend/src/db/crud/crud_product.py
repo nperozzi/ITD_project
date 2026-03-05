3196d4ca16cb26f27c114480b5616829af99c745
Nicolas Perozzi <perozzinicolas@gmail.com>
2026-03-05 21:24:11 +0100

    Created the CRUD methods for Product.

---
+++ apps/backend/src/db/crud/crud_product.py
@@ -0,0 +1,64 @@
+from typing import Any
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from db.models.product import Product
+
+
+def create_product(
+    db: Session,
+    sku: str,
+    name: str,
+    attributes_json: dict[str, Any],
+    price: float,
+) -> Product:
+    product = Product(
+        sku = sku,
+        name = name,
+        attributes_json = attributes_json,
+        price = price,
+    )
+
+    db.add(product)
+    db.commit()
+    db.refresh(product)
+
+    return product
+
+
+def get_product(db: Session, product_id: int) -> Product | None:
+    return db.get(Product, product_id)
+
+
+def get_all_products(db: Session):
+    products = select(Product)
+    return db.scalars(products).all()
+
+
+def update_product(db: Session, product_id: int, **kwargs: Any) -> Product | None:
+    product = db.get(Product, product_id)
+
+    if not product:
+        return None
+
+    for key, value in kwargs.items():
+        if hasattr(product, key):
+            setattr(product, key, value)
+
+    db.commit()
+    db.refresh(product)
+
+    return product
+
+
+def delete_product(db: Session, product_id: int) -> bool:
+    product = db.get(Product, product_id)
+
+    if not product:
+        return False
+
+    db.delete(product)
+    db.commit()
+
+    return True
