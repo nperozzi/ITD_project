1f4f703956704df446964092aa43f0cc0ca75710
Nicolas Perozzi <perozzinicolas@gmail.com>
2026-04-25 15:05:09 +0200

    changed flag in run_gateway.sh

--- apps/gateway/run_gateway.sh
+++ apps/gateway/run_gateway.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+
 docker run --rm \
-  --privileged \
-  --network host \
-  -v /var/run/dbus:/var/run/dbus \
-  -e GATEWAY_MODE=mqtt \
-  -e MQTT_BROKER=192.168.1.229 \
-  -e MQTT_PORT=1883 \
-  esl-gateway
+--privileged \
+--network host \
+-v /var/run/dbus:/var/run/dbus \
+-e GATEWAY_MODE=mqtt \
+-e MQTT_BROKER=192.168.1.229 \
+-e MQTT_PORT=1883 \
+esl-gateway
