cf9777fce4c97b1b32328b5b5b93b3866069a319
Nicolas Perozzi <perozzinicolas@gmail.com>
2026-05-05 13:03:15 +0200

    Testing mqtt brocker ip address at school

--- apps/gateway/run_gateway.sh
+++ apps/gateway/run_gateway.sh
@@ -5,6 +5,6 @@ docker run --rm \
 --network host \
 -v /var/run/dbus:/var/run/dbus \
 -e GATEWAY_MODE=mqtt \
--e MQTT_BROKER=172.19.0.2 \
--e MQTT_PORT=1883 \
+-e MQTT_BROKER=172.24.238.233 \
+-e MQTT_PORT="${MQTT_PORT:-1883}" \
 esl-gateway
