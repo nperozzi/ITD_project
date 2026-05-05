899fd3a365b23b89fbce4ee8b546215466d9a64c
Nicolas Perozzi <perozzinicolas@gmail.com>
2026-05-05 11:37:36 +0200

    change to mqtt mode

--- apps/gateway/run_gateway.sh
+++ apps/gateway/run_gateway.sh
@@ -4,7 +4,7 @@ docker run --rm \
 --privileged \
 --network host \
 -v /var/run/dbus:/var/run/dbus \
--e GATEWAY_MODE=ble-test \
+-e GATEWAY_MODE=mqtt \
 -e MQTT_BROKER=192.168.1.229 \
 -e MQTT_PORT=1883 \
 esl-gateway
